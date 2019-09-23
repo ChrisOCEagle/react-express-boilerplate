7fd2bbf79d5ee722690df2c1fd58a462b96ac3d1
ChrisOCEagle <christopherdavyydoconnell@yahoo.com>
2019-09-23 03:01:31 -0400

    added in the express server and the react frontend and bundled it all together using webpack

---
+++ client/src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../styles/styles.scss";
+
+class App extends Component {
+  render() {
+    return <h1>My React-Express Boilerplate Application</h1>;
+  }
+}
+
+export default App;
